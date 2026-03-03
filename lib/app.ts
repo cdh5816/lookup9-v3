1e80cad6237666efaa30b3d36a6bbb8c6d3e0c76
cdh5816 <cdh5816@gmail.com>
2026-03-04 00:32:29 +0900

    Update app.ts
    Signed-off-by: cdh5816 <cdh5816@gmail.com>

--- lib/app.ts
+++ lib/app.ts
@@ -3,8 +3,8 @@ import env from './env';
 
 const app = {
   version: packageInfo.version,
-  name: 'BoxyHQ',
-  logoUrl: 'https://boxyhq.com/img/logo.png',
+  name: 'LOOKUP9',
+  logoUrl: '/logo.png',   // public 폴더에 넣을 예정
   url: env.appUrl,
 };
 

350c492c9b493afb0815c5fcb154b3463382f086
cdh5816 <cdh5816@gmail.com>
2026-03-04 00:07:41 +0900

    Update next-i18next.config.js
    Signed-off-by: cdh5816 <cdh5816@gmail.com>

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ko',
+    locales: ['ko', 'en'],
   },
   localePath: path.resolve('./locales'),
 };

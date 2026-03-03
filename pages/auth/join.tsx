c2ae1ed1a74e56190870dea58a2d53a942d6d0f7
cdh5816 <cdh5816@gmail.com>
2026-03-04 01:28:11 +0900

    Update join.tsx
    Signed-off-by: cdh5816 <cdh5816@gmail.com>

--- pages/auth/join.tsx
+++ pages/auth/join.tsx
@@ -1,9 +1,5 @@
-import type { GetServerSidePropsContext } from 'next';
-
 // 회원가입 완전 차단 - 로그인 페이지로 리다이렉트
-export const getServerSideProps = async (
-  _context: GetServerSidePropsContext
-) => {
+export const getServerSideProps = async () => {
   return {
     redirect: {
       destination: '/auth/login',

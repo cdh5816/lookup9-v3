56b3969e1c3df98190d30def209877ed1def2e6c
cdh5816 <cdh5816@gmail.com>
2026-03-04 21:36:35 +0900

    Update Brand.tsx
    Signed-off-by: cdh5816 <cdh5816@gmail.com>

--- components/shared/shell/Brand.tsx
+++ components/shared/shell/Brand.tsx
@@ -1,18 +1,8 @@
 import app from '@/lib/app';
-import useTheme from 'hooks/useTheme';
 
 const Brand = () => {
-  const { theme } = useTheme();
   return (
     <div className="flex pt-6 shrink-0 items-center text-xl font-bold gap-2 dark:text-gray-100">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={theme === 'dark' ? '/logowhite.png' : app.logoUrl}
-        alt={app.name}
-        width={30}
-        height={30}
-        style={{ width: 30, height: 30, objectFit: 'contain' }}
-      />
       {app.name}
     </div>
   );

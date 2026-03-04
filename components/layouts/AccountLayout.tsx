c71246611319fb31149ff5d21d9f1d5629421f2e
cdh5816 <cdh5816@gmail.com>
2026-03-04 20:45:06 +0900

    Update AccountLayout.tsx
    Signed-off-by: cdh5816 <cdh5816@gmail.com>

--- components/layouts/AccountLayout.tsx
+++ components/layouts/AccountLayout.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import AppShell from '../shared/shell/AppShell';
-import { SWRConfig } from 'swr';
+import app from '@/lib/app';
+import { useTranslation } from 'next-i18next';
+import useTheme from 'hooks/useTheme';
 
-interface AccountLayoutProps {
+interface AuthLayoutProps {
   children: React.ReactNode;
+  heading?: string;
+  description?: string;
 }
 
-export default function AccountLayout({ children }: AccountLayoutProps) {
+export default function AuthLayout({
+  children,
+  heading,
+  description,
+}: AuthLayoutProps) {
+  const { t } = useTranslation('common');
+  const { theme } = useTheme();
+
   return (
-    <SWRConfig
-      value={{
-        revalidateOnFocus: false,
-      }}
-    >
-      <AppShell>{children}</AppShell>
-    </SWRConfig>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={theme === 'dark' ? '/logowhite.png' : app.logoUrl}
+            className="mx-auto"
+            alt={app.name}
+            width={48}
+            height={48}
+            style={{ width: 48, height: 48, objectFit: 'contain' }}
+          />
+          {heading && (
+            <p className="mt-6 text-center text-base text-gray-500 dark:text-gray-400">
+              {t(heading)}
+            </p>
+          )}
+          {description && (
+            <p className="text-center text-sm text-gray-400 dark:text-gray-500">
+              {t(description)}
+            </p>
+          )}
+        </div>
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">{children}</div>
+      </div>
+    </>
   );
 }
